3314dbde67b0ef920c4aaadf8c46bf37e25af057
natico <natamjadian.n@gmail.com>
2019-09-08 16:16:25 +0430

    change data assigment to table component

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,17 @@
 import React from "react";
 import TableView from "./TableView/TableView";
 import FormExampleCaptureValues from "./InsertTaskForm/InsertTaskForm";
-import { Grid, Image, Segment } from "semantic-ui-react";
+import { Grid, Segment } from "semantic-ui-react";
 
-const tasksObject = [
-  { id: 1, name: "test1", startTime: "2019-08-12", endTime: "2019-08-13" },
-  { id: 2, name: "test2", startTime: "2019-08-13", endTime: "2019-08-14" },
-  { id: 3, name: "test3", startTime: "2019-08-14", endTime: "2019-08-15" },
-  { id: 4, name: "test4", startTime: "2019-08-15", endTime: "2019-08-16" },
-  { id: 5, name: "test5", startTime: "2019-08-16", endTime: "2019-08-17" }
-];
+
+
 
 const App = () => (
   <React.Fragment>
     <Grid container columns={1}>
       <Grid.Column>
         <Segment>
-          <TableView tasksObject={tasksObject}>
+          <TableView >
             <p>TEST</p>
           </TableView>
         </Segment>
--- src/components/TableView/TableView.js
+++ src/components/TableView/TableView.js
@@ -4,7 +4,16 @@ import TableViewHeader from "./TableViewHeader";
 import { Table } from "semantic-ui-react";
 
 const TableView = props => {
-  const tasks = props.tasksObject;
+
+    const tasksObject = [
+        { id: 1, name: "test1", startTime: "2019-08-12", endTime: "2019-08-13" },
+        { id: 2, name: "test2", startTime: "2019-08-13", endTime: "2019-08-14" },
+        { id: 3, name: "test3", startTime: "2019-08-14", endTime: "2019-08-15" },
+        { id: 4, name: "test4", startTime: "2019-08-15", endTime: "2019-08-16" },
+        { id: 5, name: "test5", startTime: "2019-08-16", endTime: "2019-08-17" }
+      ];
+
+  const tasks = tasksObject;
   const TableBodyRow = tasks.map(task => (
     <TableViewBodyRow taskObject={task}></TableViewBodyRow>
   ));
@@ -12,7 +21,7 @@ const TableView = props => {
   return (
     <Table celled compact definition>
       <Table.Header>
-        <TableViewHeader taskObject={props.tasksObject[0]}></TableViewHeader>
+        <TableViewHeader taskObject={tasksObject[0]}></TableViewHeader>
       </Table.Header>
 
       <Table.Body>{TableBodyRow}</Table.Body>
